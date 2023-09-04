bb2173efe3a9414d08b2db9acddbdaa2cd721c48
Sourcery AI <>
2023-09-04 08:11:35 +0000

    'Refactored by Sourcery'

--- blog/before_build.py
+++ blog/before_build.py
@@ -14,31 +14,30 @@ def filter_date(issue):
     return issue.closed_at > one_month_ago
 
 def format_number(number):
-    if number > 1000:
-        return u"{:.1f}k".format(float(number) / 1000)
-    else:
-        return u"{}".format(number)
+    return (
+        u"{:.1f}k".format(float(number) / 1000)
+        if number > 1000
+        else f"{number}"
+    )
 
 with io.open("templates/auto/recent-updates.html", 'w', encoding='utf8') as recent_updates:
     recent_updates.truncate()
 
     relnotes_issues = g.search_issues("is:merged", repo="phil-opp/blog_os", type="pr", label="relnotes")[:100]
-    recent_relnotes_issues = list(filter(filter_date, relnotes_issues))
-
-    if len(recent_relnotes_issues) == 0:
-        recent_updates.write(u"No notable updates recently.")
-    else:
+    if recent_relnotes_issues := list(filter(filter_date, relnotes_issues)):
         recent_updates.write(u"<ul>\n")
 
         for pr in sorted(recent_relnotes_issues, key=lambda issue: issue.closed_at, reverse=True):
-            link = '<a href="' + pr.html_url + '">' + pr.title + "</a> "
+            link = f'<a href="{pr.html_url}">{pr.title}</a> '
             iso_date = pr.closed_at.isoformat()
             readable_date = pr.closed_at.strftime("%b&nbsp;%d")
-            datetime_str = '<time datetime="' + iso_date + '">' + readable_date + '</time>'
-            recent_updates.write(u"  <li>" + link + datetime_str + "</li>\n")
+            datetime_str = f'<time datetime="{iso_date}">{readable_date}</time>'
+            recent_updates.write(f"  <li>{link}{datetime_str}" + "</li>\n")
 
         recent_updates.write(u"</ul>")
 
+    else:
+        recent_updates.write(u"No notable updates recently.")
 repo = g.get_repo("phil-opp/blog_os")
 
 with io.open("templates/auto/stars.html", 'w', encoding='utf8') as stars:
@@ -56,7 +55,7 @@ lines = []
 year = 2020
 month = 4
 while True:
-    url = "https://rust-osdev.com/this-month/" + str(year) + "-" + str(month).zfill(2) + "/"
+    url = f"https://rust-osdev.com/this-month/{str(year)}-{str(month).zfill(2)}/"
     try:
         urllib.request.urlopen(url)
     except urllib.error.HTTPError as e:
@@ -64,8 +63,8 @@ while True:
 
     month_str = datetime.date(1900, month, 1).strftime('%B')
 
-    link = '<a href="' + url + '">This Month in Rust OSDev (' + month_str + " " + str(year) + ")</a> "
-    lines.append(u"  <li><b>" + link + "</b></li>\n")
+    link = f'<a href="{url}">This Month in Rust OSDev ({month_str} {str(year)})</a> '
+    lines.append(f"  <li><b>{link}" + "</b></li>\n")
 
     month = month + 1
     if month > 12:
